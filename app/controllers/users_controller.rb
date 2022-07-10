a49b4d5b16f59ea80216fe50f3f9ae3059cfd999
sibataichi <17e1246@u.seijo.ac.jp>
2022-07-10 01:30:06 +0000

    CRUD, コメント機能追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :profile)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  get 'home/about'
+
+  resources :posts, only: [:new, :index, :show, :edit, :create, :update, :destroy] do
+    resources :comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+
+  resources :users, only: [:index, :show, :edit, :update] do
+    resources :relationships, only: [:create, :destroy]
+    get 'follower'
+    get 'followed'
+  end
 end
